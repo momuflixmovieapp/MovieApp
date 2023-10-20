3b414849a63a8e69f2ef281b592bac90e2f2eafb
KhushiP098 <Khushbuphulara098@gmail.com>
2023-10-20 12:15:26 +0530

    first commit

---
+++ src/pages/home/topRated/TopRated.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/swithTabs/SwitchTabs'
+import useFetch from '../../../hooks/useFetch'
+import Carousel from '../../../components/carousel/Carousel'
+
+const TopRated = () => {
+  const [endpoint,setEndPoint]=useState("movie");
+  const {data,loading}=useFetch(`/${endpoint}/top_rated`)
+  
+  console.log(data);
+  
+const onTabChange=(tab)=>{
+  setEndPoint(tab==="Movies"?'movie':"tv");
+}
+
+
+  return (
+    <div className='carouselSection'>
+        <ContentWrapper>
+           <span className="carouselTitle">Top Rated</span>
+           <SwitchTabs data={["Movies","Tv Shows"]} onTabChange={onTabChange}/>
+        </ContentWrapper>
+        <Carousel data={data?.results} loading={loading} endpoint={endpoint}/>
+    </div>
+  )
+}
+
+export default TopRated;
